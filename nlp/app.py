1489007f450ee4d1dc0cc05821d6b05669dfbfe0
art <poltavskypro@gmail.com>
2020-02-15 21:09:40 +0300

    Provide function description

--- nlp/app.py
+++ nlp/app.py
@@ -1,12 +1,13 @@
 from flask import Flask,render_template, request, make_response, jsonify
 import spacy
 import sys
+sys.path.append("/home/art/Downloads/code/skyeng-grammar-filter/nlp/processing")
 import pos_tagging
 import passive_voice
 
 import time
 
-sys.path.append("/home/art/Downloads/code/skyeng-grammar-filter/nlp/processing")
+
 nlp = spacy.load('en_core_web_sm')
 
 
