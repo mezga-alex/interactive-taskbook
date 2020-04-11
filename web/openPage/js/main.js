3aa83d8dd120c1697acfdf5efec99927bcdaa643
Mezga Alexandr <mezga.studio@gmail.com>
2020-04-11 19:26:05 +0300

    Add new checker

--- web/openPage/js/main.js
+++ web/openPage/js/main.js
@@ -14,43 +14,63 @@ function checkAnswer(taskID, userAnswer) {
     return userAnswer.toUpperCase() === correctAnswer.toUpperCase();
 }
 
-$(document).ready(() => {
-    $("input").on("keydown", function checkSingleAnswer(e) {
-        if (e.keyCode == 13) {
-            // Get phrase id and phrase word id
-            let taskID = $(this).attr('id');
-            let userAnswer = $(this).val();
-            // Check if the answer is correct
-            let isCorrect = checkAnswer(taskID, userAnswer);
+// Check all words in
+function checkFullTask(e) {
+    let taskPhrasesIndices = $(e).attr('id').match(/\d+/g);
+    for (let phraseIndex of taskPhrasesIndices) {
+        var isCorrect = true;
+        var wordIndex = 0;
 
+        var taskID = '#task-' + phraseIndex.toString() + '-' + wordIndex.toString();
+        //while (isCorrect && ($(taskID).length !== 0)) {
+        while ($(taskID).length !== 0) {
+            let userAnswer = $(taskID).val();
+            isCorrect = checkAnswer(taskID, userAnswer);
+
+            // TODO: Get element by jquery.
+            //  element = $(taskID) doesn't work.
+            // Get element of the input
+            var element = document.getElementById(taskID.substr(1));
+            // If the word is correct -> set up green background
             if (isCorrect) {
-                alert('Correct');
-            } else {
-                alert('Peredelivai');
+                classie.removeClass(element, 'input__field--kaede-incorrect');
+                classie.addClass(element, 'input__field--kaede-correct');
+
+             // If the word is correct and is not empty -> set up red background
+            } else if (userAnswer !== '') {
+                classie.removeClass(element, 'input__field--kaede-correct');
+                classie.addClass(element, 'input__field--kaede-incorrect');
+            }
+
+            // Next ID
+            wordIndex += 1;
+            taskID = '#task-' + phraseIndex.toString() + '-' + wordIndex.toString();
+        }
+    }
+}
+
+$(document).ready(() => {
+    // Handle pressing the enter key
+    var inputs = $(':input').keypress(function(e){
+        if (e.which == 13) {
+            e.preventDefault();
+            var nextInput = inputs.get(inputs.index(this) + 1);
+            if (nextInput) {
+                // If the next element is an INPUT -> switch to the next input.
+                if (nextInput.tagName === 'INPUT') {
+                    nextInput.focus();
+                }
+                // If the next element is the Button -> check the task by emulating the button click.
+                else {
+                    let nextID = '#' + $(nextInput).attr('id');
+                    $(nextID).click();
+                }
             }
         }
     });
 
+    // Get the button and check all related words inside the task
     $('.btn-check-task').on('click', function checkMultipleAnswers(e) {
-        let taskPhrasesIndices = $(this).attr('id').match(/\d+/g);
-        for (let phraseIndex of taskPhrasesIndices) {
-            var isCorrect = true;
-            var wordIndex = 0;
-            var taskID = '#task-' + phraseIndex.toString() + '-' + wordIndex.toString();
-            //while (isCorrect && ($(taskID).length !== 0)) {
-            while ($(taskID).length !== 0) {
-                let userAnswer = $(taskID).val();
-                isCorrect = checkAnswer(taskID, userAnswer);
-
-                if (isCorrect) {
-                    alert(taskID + ' Correct');
-                } else {
-                    alert(taskID + ' Peredelivai');
-                }
-
-                wordIndex += 1;
-                taskID = '#task-' + phraseIndex.toString() + '-' + wordIndex.toString();
-            }
-        }
+        checkFullTask(this);
     });
 });
