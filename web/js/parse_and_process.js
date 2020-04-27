474ee610c923a16612f9a73db8e130f8890d7160
Mezga Alexandr <mezga.studio@gmail.com>
2020-04-27 23:12:16 +0300

    Add not 'Null' check

--- web/js/parse_and_process.js
+++ web/js/parse_and_process.js
@@ -1,4 +1,3 @@
-
 var PARSE_UTILS = {
     minBodyTailLength: function () {
         return 100;
@@ -126,42 +125,21 @@ $("#switch-id").click(function() {
 
 $("#btn-find").on("click", () => {
 
-    // //////////////////////////////////////////////
-    // // TODO: Update for the new visualization
-    // let pos = $('#POS').val();
-    // let active_voice = $('#ACTIVE_VOICE').val();
-    // let passive_voice = $('#PASSIVE_VOICE').val();
-    // let task = '';
-    // let specifiedTask = '';
-    //
-    // if (pos !== 'NONE')  {
-    //     task = 'POS';
-    //     specifiedTask = pos;
-    // } else if (active_voice !== 'NONE')  {
-    //     task = 'ACTIVE_VOICE';
-    //     specifiedTask = active_voice;
-    // }
-    // else if (passive_voice !== 'NONE')  {
-    //     task = 'PASSIVE_VOICE';
-    //     specifiedTask = passive_voice;
-    // } else {
-    //     alert('Specify the task');
-    //     return false;
-    // }
-    // //////////////////////////////////////////////
     let task = $('#task').val();
     let specifiedTask = $('#specifiedTask').val();
-    alert(task + ' ' + specifiedTask);
-    chrome.tabs.getSelected(null, (tab) => {
-        let tabUrl = tab.url;
-        PARSE_UTILS.keywordInterval(tabUrl, (text) => {
-            newTaskRequest(server, text, task, specifiedTask).then(function() {
-                localStorage.setItem('server', server);
-                chrome.tabs.create({'url': './openPage/result.html'}, (tab) => {});
-            }).catch(function () {
-                alert('No matches found')
-            });
+    if (task !== 'NONE' && specifiedTask !== 'NONE') {
+        chrome.tabs.getSelected(null, (tab) => {
+            let tabUrl = tab.url;
+            PARSE_UTILS.keywordInterval(tabUrl, (text) => {
+                newTaskRequest(server, text, task, specifiedTask).then(function () {
+                    localStorage.setItem('server', server);
+                    chrome.tabs.create({'url': './openPage/result.html'}, (tab) => {
+                    });
+                }).catch(function () {
+                    alert('No matches found')
+                });
 
+            });
         });
-    });
+    }
 });
