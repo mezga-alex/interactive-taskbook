9c7d340d95c1868f6c6b106ce0e7275ac18c64a0
Artem <Poltavsky>
2020-05-04 19:30:55 +0300

    update db connection

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ cluster = MongoClient("mongodb+srv://arch:lbpfqycfrctc@itb-xeeen.mongodb.net/tes
 arch_home = '/home/art/Downloads/code_g/skyeng-grammar-filter/nlp/processing'
 arch_work = "/home/art/Downloads/pipe/skyeng-grammar-filter/nlp/processing"
 
-sys.path.append(arch_home)
+sys.path.append(arch_work)
 import text_processor as tp
 import pos_tagging
 import active_voice
@@ -26,168 +26,38 @@ app = Flask(__name__)
 db = cluster.itb
 collection = db.users
 
-"""
-"statistics": [{
-        "article_links": [{
-            "url": "",
-            "exercise": [{
-                "type": "",
-                "words": [{
-                    "value": "",
-                    "correct": 0,
-                    "wrong": 0,
-                    "pos": "",
-                }],
-            }],
-        }],
-    }],
-    
-sample_user = {"user": "arch",
-               "124tgfvsc": {
-                   "article_links": {
-                       "sample_com":
-                           {
-                               # "url": "sample.com",
-                               # "exercise": {
-                               "passive_all":
-                                   {
-                                       # "type": "passive_all",
-                                       "words": {
-                                           "1": {
-                                               "value": "bruh",
-                                               "correct": 1,
-                                               "wrong": 14,
-                                               "pos": "adj",
-                                           },
-                                           "2": {
-                                               "value": "Sanya",
-                                               "correct": 1,
-                                               "wrong": 6,
-                                               "pos": "noun",
-                                           },
-                                       },
-
-                                   },
-                           },
-                       # },
-                   },
-               },
-               }
-"""
 
 sample_user = {
     "session_id": "iiimfbnmnldgfepajlnghaccejcaiaik",
     "statistics": [],
 }
 
-# session_0 = {
-#             "sample_com":
-#                 {
-#                     "passive_all":
-#                         {
-#                             "words": {
-#                                 "1": {
-#                                     "value": "bruh",
-#                                     "correct": 1,
-#                                     "wrong": 14,
-#                                     "pos": "adj",
-#                                 },
-#                                 "2": {
-#                                     "value": "Sanya",
-#                                     "correct": 1,
-#                                     "wrong": 6,
-#                                     "pos": "noun",
-#                                 },
-#                             },
-#
-#                         },
-#                 },
-#             }
 
+@app.route('/get_db', methods=["GET", "POST"])
+def get_user():
+    try:
+        # connect to json
 
-# active = {
-#              "article_links": {
-#                  "sample_com":
-#                      {
-#                          # "url": "sample.com",
-#                          # "exercise": {
-#                          "pos_all":
-#                              {
-#                                  # "type": "passive_all",
-#                                  "words": {
-#                                      "1": {
-#                                          "value": "bruh",
-#                                          "correct": 1,
-#                                          "wrong": 14,
-#                                          "pos": "adj",
-#                                      },
-#                                      "2": {
-#                                          "value": "Sanya",
-#                                          "correct": 1,
-#                                          "wrong": 6,
-#                                          "pos": "noun",
-#                                      },
-#                                  },
-#
-#                              },
-#                      },
-#                  # },
-#              },
-#          },
-
-active_all = {
-                 # "type": "passive_all",
-                 "words": {
-                     "1": {
-                         "value": "bruh",
-                         "correct": 1,
-                         "wrong": 14,
-                         "pos": "adj",
-                     },
-                     "2": {
-                         "value": "Sanya",
-                         "correct": 1,
-                         "wrong": 6,
-                         "pos": "noun",
-                     },
-                 },
-             },
-
-active_all_0 = {
-                   # "type": "passive_all",
-                   "words": {
-                       "1": {
-                           "value": "rgdfvh",
-                           "correct": 1,
-                           "wrong": 14,
-                           "pos": "adj",
-                       },
-                       "2": {
-                           "value": "Sasdfsdfya",
-                           "correct": 1,
-                           "wrong": 6,
-                           "pos": "noun",
-                       },
-                   },
-               },
-
-
-# @app.route("/add_user", methods=["POST"])
-# def add_user():
-#     try:
-#         # req = request.get_json()
-#         # print(req)
-#         collection.insert(active_all)
-#     except request.exceptions as e:
-#         print('request error occured: ', e)
-#         return make_response(jsonify(result=[e]), 500)
+        req = request.get_json()
+        print(req)
+        return make_response(jsonify(200))
+    except request.exceptions as e:
+        print('request error occured: ', e)
+        return make_response(jsonify(result=[e]), 500)
 
 
 @app.route('/update', methods=["POST"])
 def update_user():
     try:
         req = request.get_json()
-        print(req)
+        # print(req)
+        session_id = req['extensionID']
+        _json = req['json']
+        stats = _json['statistics']
+        print(session_id)
+        print(stats)
+        update_db(session_id, stats)
+        return make_response(jsonify(200))
     except request.exceptions as e:
         print('request error occured: ', e)
         return make_response(jsonify(result=[e]), 500)
@@ -229,29 +99,24 @@ def answer():
     return render_template("index.html", results=results, num_of_results=len(results), text=text)
 
 
-def insert_db():
+def add_user(session_id):
+    sample_user = {
+        "session_id": session_id,
+        "statistics": [],
+    }
     collection.insert_one(sample_user)
 
 
 def update_db(session_id, stats):
-    results = collection.find("session_id")
-    sample_user = {
-        "session_id": "iiimfbnmnldgfepajlnghaccejcaiaik",
-        "statistics": [],
-    }
-    for result in results:
-        print(result)
-    # collection.update_one({"user": "arch"}, {"$set": {".".join([session, links, site, exercise]): active_all}})
+    results = list(collection.find({"session_id": session_id}))
+    print("result", type(results))
+    # print(results[0])
+
+    if len(results) == 0:
+        add_user(session_id)
+
     collection.update_one({"session_id": session_id}, {"$push": {"statistics": stats}})
 
-    # collection.update_one({"user":"arch"}, {"$set": {session+"."+links+"."+site+"."+exercise: active_all}})
-    # collection.update_one({"user": "arch"}, {"$set": {session: active}})
-
-
-# /{"article_links": "sample_com"}
 
 if __name__ == '__main__':
-    insert_db()
-    update_db()
-    print('inserted')
     app.run(host='0.0.0.0', port=8050)
