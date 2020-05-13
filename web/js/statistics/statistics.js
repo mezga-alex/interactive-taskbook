74fc8b507810385e0c0f27e3778e440b0bfceeb5
Mezga Alexandr <mezga.studio@gmail.com>
2020-05-14 02:14:30 +0300

    Update statistics

--- web/js/statistics/statistics.js
+++ web/js/statistics/statistics.js
@@ -1,8 +1,24 @@
+function noResultsVisualization() {
+    const text = "Hmm... It looks like you haven't completed tasks of this type yet."
+    const html = '<img class="mx-auto d-block" style="width: 25rem; margin-top: 1rem; margin-bottom: 1rem;" ' +
+                 'src="../img/undraw_screen_time_vkev.svg" alt="">' +
+                  '<p class="mb-4 text-center">' + text + '</p>';
+
+    $("#chart-body").empty()
+    $("#chart-body").append(html);
+}
+
 // Count statistics with Wrong/Correct answers
-function countAnswersStatistics(statisticsJSON, tasks, specifiedTask) {
+function countAnswersStatistics(statisticsJSON, tasks, specifiedTasks) {
     if (tasks = 'ALL')
         tasks = new Set(['PASSIVE_VOICE', 'ACTIVE_VOICE']);
     else tasks = new Set([tasks]);
+
+    if (!Array.isArray(specifiedTasks))
+        specifiedTasks = [specifiedTasks];
+
+    specifiedTasks = new Set(specifiedTasks);
+
     var correctAnswers = 0;
     var wrongAnswers = 0;
 
@@ -10,7 +26,7 @@ function countAnswersStatistics(statisticsJSON, tasks, specifiedTask) {
         for (const exercise of articleStatistics.exercises) {
             if (tasks.has(exercise.task)) {
                 for (const specification of exercise.specifications) {
-                    if (specification.specifiedTask === specifiedTask || specifiedTask === 'ALL') {
+                    if (specifiedTasks.has(specification.specifiedTask) || specifiedTasks.has('ALL')) {
                         for (const word of specification.words) {
                             correctAnswers += word.correct;
                             wrongAnswers += word.wrong;
@@ -38,7 +54,8 @@ function visualizeStatistics(statisticsJSON, tasks='ALL', specifiedTask = 'ALL',
         }
     }
     catch(e) {
-        alert('Hm. Something happened to your stats. Reload the page.');
+        noResultsVisualization();
+//         alert("Hmm... It looks like you haven't completed tasks of this type yet.");
         console.log(e);
     }
 }
