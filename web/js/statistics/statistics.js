fad4b990a157d7d7ce9f0503094916ce80093b56
Mezga Alexandr <mezga.studio@gmail.com>
2020-05-14 16:06:56 +0300

    Major Statistics Update

--- web/js/statistics/statistics.js
+++ web/js/statistics/statistics.js
@@ -1,27 +1,39 @@
+// Add canvas element
+function addCanvas() {
+    const canvasHTML = '<div class="chart-pie">' +
+                       '<canvas id="myPieChart"></canvas>' +
+                       '</div>' +
+                       '<div class="mt-4 text-center small">' +
+                       '<span class="mr-2">' +
+                       '<i class="fas fa-circle text-success"></i> Correct</span>' +
+                       '<span class="mr-2">' +
+                       '<i class="fas fa-circle text-danger"></i> Wrong</span>' +
+                       '</div>'
+
+    $("#chart-body").empty();
+    $("#chart-body").append(canvasHTML);
+}
 function noResultsVisualization() {
     const text = "Hmm... It looks like you haven't completed tasks of this type yet."
     const html = '<img class="mx-auto d-block" style="width: 25rem; margin-top: 1rem; margin-bottom: 1rem;" ' +
                  'src="../img/undraw_screen_time_vkev.svg" alt="">' +
                   '<p class="mb-4 text-center">' + text + '</p>';
 
-    $("#chart-body").empty()
+    $("#chart-body").empty();
     $("#chart-body").append(html);
 }
 
 // Count statistics with Wrong/Correct answers
 function countAnswersStatistics(statisticsJSON, tasks, specifiedTasks) {
-    if (tasks = 'ALL')
+    if (tasks.size === 0)
         tasks = new Set(['PASSIVE_VOICE', 'ACTIVE_VOICE']);
-    else tasks = new Set([tasks]);
-
-    if (!Array.isArray(specifiedTasks))
-        specifiedTasks = [specifiedTasks];
-
-    specifiedTasks = new Set(specifiedTasks);
+    if (specifiedTasks.size === 0)
+        specifiedTasks.add('ALL')
 
     var correctAnswers = 0;
     var wrongAnswers = 0;
 
+    console.log(tasks, specifiedTasks)
     for (const articleStatistics of statisticsJSON.statistics) {
         for (const exercise of articleStatistics.exercises) {
             if (tasks.has(exercise.task)) {
@@ -41,26 +53,107 @@ function countAnswersStatistics(statisticsJSON, tasks, specifiedTasks) {
 
 // Main plotting function
 // TODO: Add another Charts
-function visualizeStatistics(statisticsJSON, tasks='ALL', specifiedTask = 'ALL', typeOfVisualization='pie', elementID="myPieChart") {
+function visualizeStatistics(statisticsJSON, tasks=new Set(), specifiedTask = new Set(), typeOfVisualization='pie', elementID="myPieChart") {
     try {
         switch (typeOfVisualization) {
             case 'pie':
                 const dataValue = countAnswersStatistics(statisticsJSON, tasks, specifiedTask);
-                createPieChart(dataValue);
+                console.log(dataValue);
+
+                if ((dataValue[0] + dataValue[1]) !== 0) {
+                    addCanvas();
+                    createPieChart(dataValue);
+                } else noResultsVisualization();
                 break;
             default:
                 console.log('ERROR PLOTTING');
+                noResultsVisualization();
                 break;
         }
     }
     catch(e) {
         noResultsVisualization();
-//         alert("Hmm... It looks like you haven't completed tasks of this type yet.");
         console.log(e);
     }
 }
 
+// Take parameters from the selector
+function getChartParameters() {
+    var tasks = new Set();
+    var specifiedTasks = new Set();
+    var selectorSet = new Set($("#tasksSelector").val());
+    if (selectorSet.has("ACTIVE_VOICE")) {
+        tasks.add("ACTIVE_VOICE");
+        selectorSet.delete("ACTIVE_VOICE");
+    }
+    if (selectorSet.has("PASSIVE_VOICE")) {
+        tasks.add("PASSIVE_VOICE");
+        selectorSet.delete("PASSIVE_VOICE");
+    }
+    specifiedTask = selectorSet;
+
+    console.log('Tasks: ', tasks, tasks.size);
+    console.log('SpecifiedTasks: ', specifiedTask, specifiedTask.size);
+    return [tasks, specifiedTask];
+}
+
+// Handle build-button click
+function buildChartButtonControl() {
+    $("#build-chart-btn").on("click", () => {
+        var globalStatisticsJSON = JSON.parse(localStorage.getItem("globalStatisticsJSON"));
+        if (!globalStatisticsJSON) {
+            getDataBaseJSON(server+'/get_data', extensionID).then(function(value) {
+                        console.log(value,': Response received');
+                        globalStatisticsJSON = JSON.parse(localStorage.getItem("globalStatisticsJSON"));
+                        const parameters = getChartParameters();
+                        const tasks = parameters[0];
+                        const specifiedTasks = parameters[1];
+
+                        visualizeStatistics(globalStatisticsJSON, tasks, specifiedTasks)
+                    }, function(reason) {
+                        console.log(reason, ': No results');
+
+                        // Simulate error
+                        visualizeStatistics(null, null, null);
+                    });
+        } else {
+            const parameters = getChartParameters();
+            const tasks = parameters[0];
+            const specifiedTasks = parameters[1];
+
+            visualizeStatistics(globalStatisticsJSON, tasks, specifiedTasks)
+        }
+    });
+}
+
+// Selector handler
+function selectorControl() {
+    $("#tasksSelector").change(function(){
+        const tasksSet = new Set($("#tasksSelector").val());
+        console.log(tasksSet);
+        if (tasksSet.has('ACTIVE_VOICE') || tasksSet.has('PASSIVE_VOICE')) {
+            $( "#specifiedTasks" ).prop( "disabled", false );
+            if (tasksSet.has('PASSIVE_VOICE')) {
+                $( ".passiveOnly" ).prop( "disabled", false );
+            } else {
+                $( ".passiveOnly" ).prop( "disabled", true );
+            }
+            $("#tasksSelector").selectpicker("refresh");
+
+        } else {
+            $( "#specifiedTasks" ).prop( "disabled", true );
+            $("#tasksSelector").selectpicker("refresh");
+        }
+    })
+}
+
+// Initialize control handlers
+function handleControlElements() {
+    selectorControl();
+    buildChartButtonControl();
+}
+
 $(document).ready(() => {
-    const globalStatisticsJSON = JSON.parse(localStorage.getItem("globalStatisticsJSON"));
-    visualizeStatistics(globalStatisticsJSON)
+    handleControlElements();
+    $("#build-chart-btn").trigger("click");
 });
